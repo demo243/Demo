309e09045a46bde2a1fc844255eb68905f6b0410
iswarya lakshmi <iswarya@fabric.inc>
2021-10-23 15:12:59 +0530

    Stashed changes demo, mongoose setup

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose');
 
 const app = express()
 const port = 3200
 
-
-app.get('/contact', (req, res) => { //home page
-  res.send('Hello World!')
+mongoose.connect('mongodb://localhost:27017/demo', {useNewUrlParser: true, useUnifiedTopology : true})
+.then((connection) => {
+  console.log("connected to DB --->>>")
+  
+  app.get('/contact', (req, res) => { //home page
+    res.send('Hello World!')
+  })
 })
+.catch((error) => {
+  console.log(`Error while connecting to DB: ${error}`)
+  mongoose.disconnect()
+  throw error
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
