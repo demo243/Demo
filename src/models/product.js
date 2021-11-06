7506a441be8d474ecb8ae5993ed6eb1878e14ec3
iswarya lakshmi <iswarya@fabric.inc>
2021-11-06 12:22:25 +0530

    how to raise pr demo - CRUD API

---
+++ src/models/product.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const { Category } = require('./category');
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+		name: {
+			type: String,
+			required: true,
+			minLength: 2,
+			maxLength: 130
+		},
+    category: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: Category
+    },
+		price: {
+			type: Number,
+			required: true,
+			min:1
+		},
+		description: {
+			type: String,
+			required: true,
+			minLength: 5,
+			maxLength: 10000
+		},
+		stock: {
+			type: Number,
+			required: true,
+			min: 0
+		},
+		codEligible: {
+			type: Boolean,
+			required: true,
+			enum: [true, false],
+			default: true
+		},
+		code: {
+			type: String,
+			required: true
+		}
+})
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = {
+    Product
+}
