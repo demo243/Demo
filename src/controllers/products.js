9b123b981e5996293b30d3d94174ea952f32ae18
iswarya lakshmi <iswarya@fabric.inc>
2021-12-15 10:58:38 +0530

    products CRUD apis operations

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,22 +1,128 @@
+const Product = require('../models/product')
+
 const createProducts = (request, response) => {
 	console.log(request.body)
-	response.send(" created Products ")
+	const productRequest = new Product(request.body)
+	productRequest.save()
+	.then(saveProduct => {
+		return response.status(201).json({
+			status: true,
+			data: saveProduct
+		})
+	})
+	.catch(error => {
+		return response.status(500).json({
+			status: false,
+			error: {
+				name: error.name,
+				message: error.message
+			}
+		})
+	})
+	// response.send(" created Products ")
 }
 
 const updateProducts =  (request, response) => {
-	console.log(request.params.productId)
-	response.send("Updated Products")
+	// request.params.productId // errror // update response
+	Product.findById(request.params.productId)
+	.then(product => {
+		if(!product) {
+			return response.status(404).json({
+				status: false,
+				error: {
+					name: 'NotFoundError',
+					message: 'Product with this Id not found'
+				}
+			})
+		}
+		product.set(request.body)
+		product.save()
+		.then(updatedProduct => {
+			return response.status(200).json({
+				status: true,
+				data: updatedProduct
+			})
+		})
+		.catch(error => {
+		return response.status(500).json({
+			status: false,
+			error: {
+				name: error.name,
+				message: error.message
+			}
+		})
+	})
+	})
+	.catch(error => {
+		return response.status(500).json({
+			status: false,
+			error: {
+				name: error.name,
+				message: error.message
+			}
+		})
+	})
+	// console.log(request.params.productId)
+	// response.send("Updated Products")
 }
 
 const deleteProducts =  (request, response) => {
-	console.log(request.params.productId)
-	response.send("Deleted Products")
+	Product.findById(request.params.productId)
+	.then(product => {
+		if(!product) {
+			return response.status(404).json({
+				status: false,
+				error: {
+					name: 'NotFoundError',
+					message: 'Product with this Id not found'
+				}
+			})
+		}
+		product.remove()
+		.then(deletedProduct => {
+			return response.status(200).json({
+				status: true,
+				data: deletedProduct
+			})
+		})
+		.catch(error => {
+			return response.status(500).json({
+				status: false,
+				error: {
+					name: error.name,
+					message: error.message
+				}
+			})
+		})
+	})
+	.catch(error => {
+		return response.status(500).json({
+			status: false,
+			error: {
+				name: error.name,
+				message: error.message
+			}
+		})
+	})
 }
 
 const listProducts =  (request, response) => {
-	console.log(request.query.limit)
-  console.log(request.query.offset)
-	response.send("List Products")
+	Product.find()
+	.then(listProducts => {
+		return response.status(200).json({
+			status: true,
+			data: listProducts
+		})
+	})
+	.catch(error => {
+		return response.status(500).json({
+			status: false,
+			error: {
+				name: error.name,
+				message: error.message
+			}
+		})
+	})
 }
 
 module.exports = {
